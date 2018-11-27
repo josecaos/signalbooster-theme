23235403affdc3ee8e4408998332507ee72c943a
josecaos <jazzvoon@gmail.com>
2018-11-27 14:56:52 -0600

    Dependencias + nuevos archivos

--- Assets/main.js
+++ Assets/main.js
@@ -1,6 +1,29 @@
-$(document).ready({
+$(document).ready(() => {
 
-  alert('SIGNALBOOSTER')
+  console.log('SIGNALBOOSTER DEV\n' + ' ///////////////////////')
+//
+
+  img_liquid()
+
+
+//
+  console.log('Scripts OK')
 
 })
 //
+function img_liquid() {
+
+	$('.imgLiquid .imgLiquidFill').imgLiquid()
+    $('.imgLiquid .imgLiquidNoFill').imgLiquid({
+      fill: false
+    })
+    $('.imgLiquid .imgLiquidNoFillLeft').imgLiquid({
+      fill: false,
+      horizontalAlign: "left"
+    })
+    $('.imgLiquid .imgLiquidNoFillRight').imgLiquid({
+          fill: false,
+      horizontalAlign: "right"
+    })
+
+}
